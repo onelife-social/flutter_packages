8f310f76569de9da264dc85c65564f199a38c706
juanan-onelife <juanan.garcia@onelife.social>
2024-11-19 09:10:33 +0100

    Revert "add prints"
    This reverts commit 8c311d829dbdc8cc32a1cc429bcc2ea5e5057434.

--- packages/file_selector/file_selector_web/lib/src/dom_helper.dart
+++ packages/file_selector/file_selector_web/lib/src/dom_helper.dart
@@ -66,10 +66,8 @@ class DomHelper {
 
     // TODO(dit): Reimplement this with the showPicker() API, https://github.com/flutter/flutter/issues/130365
     try {
-      print('inputElement.showPicker()');
       inputElement.showPicker();
     } catch (e) {
-      print('inputElement.click');
       inputElement.click();
     }
 
